002da6c35d57d438e5ef267551b091d237c428da
Patrick Santos <patrickspteixeira@gmail.com>
2020-04-19 18:01:02 -0300

    altera estilos do cabecalho

--- vagas-frontend/src/components/VagaItem/VagaItem.js
+++ vagas-frontend/src/components/VagaItem/VagaItem.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import { CardHeader } from '@material-ui/core';
 
 function VagaItem({ item }) {
   return (
     <Card>
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {item.company.name} | {item.company.province}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            E-mail: {item.jobRecruiter.email}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <CardHeader
+        title={`${item.company.name} | ${item.company.province}`}
+        subheader={`E-mail: ${item.jobRecruiter.email}`}
+      />
       <CardActions>
         <Button
           target="_blank"
